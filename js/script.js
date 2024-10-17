6b04d83e2839ac5d36e1e180282445f3ea80e200
feryanuar24 <feryanuar24@gmail.com>
2024-10-18 00:29:18 +0700

    feat: detail rumah page with url parameter

--- js/script.js
+++ js/script.js
@@ -29,6 +29,11 @@ const tipeRumahData = [
     kamar_mandi: "2",
     kamar_tidur: "3",
     img: "/assets/images/rumah-tipe-1.png",
+    harga: "700",
+    carport: "1",
+    kanopi: "Free",
+    pompa_air: "Free",
+    toren_air: "Free",
   },
   {
     tipe: "Rumah Klasik",
@@ -39,6 +44,11 @@ const tipeRumahData = [
     kamar_mandi: "3",
     kamar_tidur: "4",
     img: "/assets/images/rumah-tipe-2.png",
+    harga: "1000",
+    carport: "2",
+    kanopi: "Free",
+    pompa_air: "Free",
+    toren_air: "Free",
   },
   {
     tipe: "Rumah Konteporer",
@@ -49,6 +59,11 @@ const tipeRumahData = [
     kamar_mandi: "2",
     kamar_tidur: "3",
     img: "/assets/images/rumah-tipe-3.png",
+    harga: "800",
+    carport: "2",
+    kanopi: "Free",
+    pompa_air: "Free",
+    toren_air: "Free",
   },
   {
     tipe: "Rumah Skadinavia",
@@ -59,6 +74,11 @@ const tipeRumahData = [
     kamar_mandi: "2",
     kamar_tidur: "3",
     img: "/assets/images/rumah-tipe-4.png",
+    harga: "900",
+    carport: "1",
+    kanopi: "Free",
+    pompa_air: "Free",
+    toren_air: "Free",
   },
 ];
 
@@ -78,26 +98,36 @@ const renderTipeRumahArticle = () => {
             <div class="tipe-rumah-container-group">
               <div class="tipe-rumah-container-item">
                 <i class="fa-solid fa-building tipe-rumah-icon"></i>
-                <h4>Luas Bangunan</h4>
-                <p>${tipeRumah.luas_bangunan} m<sup>2</sup></p>
+                <div>
+                  <h4>Luas Bangunan</h4>
+                  <p>${tipeRumah.luas_bangunan} m<sup>2</sup></p>
+                </div>
               </div>
               <div class="tipe-rumah-container-item">
                 <i class="fa-solid fa-landmark tipe-rumah-icon"></i>
-                <h4>Luas Tanah</h4>
-                <p>${tipeRumah.luas_tanah} m<sup>2</sup></p>
+                <div>
+                  <h4>Luas Tanah</h4>
+                  <p>${tipeRumah.luas_tanah} m<sup>2</sup></p>
+                </div>
               </div>
               <div class="tipe-rumah-container-item">
                 <i class="fa-solid fa-restroom tipe-rumah-icon"></i>
-                <h4>Kamar Mandi</h4>
-                <p>${tipeRumah.kamar_mandi}</p>
+                <div>
+                  <h4>Kamar Mandi</h4>
+                  <p>${tipeRumah.kamar_mandi}</p>
+                </div>
               </div>
               <div class="tipe-rumah-container-item">
                 <i class="fa-solid fa-bed tipe-rumah-icon"></i>
-                <h4>Kamar Tidur</h4>
-                <p>${tipeRumah.kamar_tidur}</p>
+                <div>
+                  <h4>Kamar Tidur</h4>
+                  <p>${tipeRumah.kamar_tidur}</p>
+                </div>
               </div>
             </div>
-            <button class="btn-selengkapnya">Selengkapnya</button>
+            <button class="btn-selengkapnya" onclick="detailRumah('${tipeRumah.tipe
+              .toLowerCase()
+              .replace(/\s+/g, "-")}')">Selengkapnya</button>
           </div>
           <figure>
           ${
@@ -182,6 +212,127 @@ const renderFasilitasArticle = () => {
   });
 };
 
+// Redirect to Detail Rumah
+const detailRumah = (tipe) => {
+  window.location.href = `detail-rumah.html?tipe=${tipe}`;
+};
+
+// Get URL Parameter
+const getParameterByName = (name) => {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(name);
+};
+
+const detailRumahContainer = document.getElementById("detail-rumah-container");
+
+const renderDetailRumah = () => {
+  const tipe = getParameterByName("tipe");
+  const selectedTipeRumah = tipeRumahData.find(
+    (tipeRumah) => tipe === tipeRumah.tipe.toLowerCase().replace(/\s+/g, "-")
+  );
+  const unselectedTipeRumah = tipeRumahData.filter(
+    (tipeRumah) => tipe !== tipeRumah.tipe.toLowerCase().replace(/\s+/g, "-")
+  );
+
+  detailRumahContainer.innerHTML = `
+    <section class="banner">
+        <h2>${selectedTipeRumah.tipe}</h2>
+      </section>
+      <section class="detail-rumah-main">
+        <img
+          src="assets/images/rumah-tipe-1.png"
+          alt="${selectedTipeRumah.tipe}"
+          class="detail-rumah-img"
+        />
+        <div class="detail-rumah-text-group">
+          <h3 class="detail-rumah-title">${selectedTipeRumah.tipe}</h3>
+          <p>
+            ${selectedTipeRumah.detail}
+          </p>
+          <div class="detail-rumah-grid">
+            <div class="detail-rumah-item">
+              <i class="fa-solid fa-dollar-sign"></i>
+              <p>Harga: ${selectedTipeRumah.harga} Juta-an</p>
+            </div>
+            <div class="detail-rumah-item">
+              <i class="fa-solid fa-landmark"></i>
+              <p>Luas Tanah: ${selectedTipeRumah.luas_tanah} m<sup>2</sup></p>
+            </div>
+            <div class="detail-rumah-item">
+              <i class="fa-solid fa-building"></i>
+              <p>Luas Bangunan: ${selectedTipeRumah.luas_bangunan} Lantai</p>
+            </div>
+            <div class="detail-rumah-item">
+              <i class="fa-solid fa-shower"></i>
+              <p>Kamar Mandi: ${selectedTipeRumah.kamar_mandi}</p>
+            </div>
+            <div class="detail-rumah-item">
+              <i class="fa-solid fa-bed"></i>
+              <p>Kamar Tidur: ${selectedTipeRumah.kamar_tidur}</p>
+            </div>
+            <div class="detail-rumah-item">
+              <i class="fa-solid fa-warehouse"></i>
+              <p>Garasi: ${selectedTipeRumah.carport}</p>
+            </div>
+            <div class="detail-rumah-item">
+              <i class="fa-solid fa-umbrella-beach"></i>
+              <p>Kanopi: ${selectedTipeRumah.kanopi}</p>
+            </div>
+            <div class="detail-rumah-item">
+              <i class="fa-solid fa-water"></i>
+              <p>Pompa Air: ${selectedTipeRumah.pompa_air}</p>
+            </div>
+            <div class="detail-rumah-item">
+              <i class="fa-solid fa-tint"></i>
+              <p>Toren Air: ${selectedTipeRumah.toren_air}</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="detail-rumah-kontak">
+        <img src="assets/images/denah.jpg" alt="Denah Rumah" class="detail-rumah-denah">
+        <div class="detail-rumah-kontak-text">
+          <h2 class="detail-rumah-kontak-title">Kontak</h2>
+          <p>Hubungi kami untuk selanjutnya</p>
+          <div class="kontak-grid">
+            <a class="kontak-item" href="https://api.whatsapp.com/send/?phone=083813479771&amp;text=Hello
+            ">
+              <i class="fa-solid fa-phone"></i>
+              <p>0812-3456-7890</p>
+            </a>
+            <a class="kontak-item" href="mailto:feryanuar24@gmail.com">
+              <i class="fa-solid fa-envelope"></i>
+              <p>feryanuar24@gmail.com</p>
+            </a>
+        </div>
+      </section>
+      <section class="lainnya">
+        <h2>Tipe Rumah Lainnya</h2>
+        <div class="lainnya-grid" id="lainnya-grid">
+        </div>
+      </section>
+  `;
+
+  const lainnyaGrid = document.getElementById("lainnya-grid");
+
+  unselectedTipeRumah.forEach((tipeRumah) => {
+    lainnyaGrid.innerHTML += `
+      <div class="lainnya-item">
+            <img
+              src="${tipeRumah.img}"
+              alt="${tipeRumah.tipe}"
+              class="lainnya-img"
+            />
+            <a href="detail-rumah.html?tipe=${tipeRumah.tipe
+              .toLowerCase()
+              .replace(/\s+/g, "-")}" class="link-to-lainnya">${
+      tipeRumah.tipe
+    }</a>
+          </div>
+    `;
+  });
+};
+
 // Render Tipe Rumah and Fasilitas Article
 document.addEventListener("DOMContentLoaded", () => {
   if (document.getElementById("all-tipe-rumah-container")) {
@@ -189,5 +340,8 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   if (document.getElementById("fasilitas-list")) {
     renderFasilitasArticle();
+  }
+  if (document.getElementById("detail-rumah-container")) {
+    renderDetailRumah();
   }
 });
